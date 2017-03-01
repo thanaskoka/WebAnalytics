972d81daa8c270cd2765707ed1b9dd206fcff07b
koka <Cefriel-20!7>
2017-03-01 15:30:24 +0100

    add log analysis, helper functions and complete log enrichment class

--- src/main/scala/com/webanalytics/analysis/LogEnrichment.scala
+++ src/main/scala/com/webanalytics/analysis/LogEnrichment.scala
@@ -1,8 +1,9 @@
 package com.webanalytics.analysis
 
 import com.webanalytics.config._
+import com.webanalytics.helper._
 import org.apache.spark.SparkContext
-
+import org.apache.spark.sql.functions._
 /**
   * Created by Thanas Koka on 21/02/2017.
   */
@@ -12,40 +13,86 @@ object LogEnrichment extends DataPreparation {
   def main(args: Array[String]): Unit = {
 
 /*  //run as spark standalone mode
-    val conf= new SparkConf().setAppName("AutobusAnalysis").setMaster("local")
+    val conf= new SparkConf().setAppName("WebAnalytics").setMaster("local")
     val sc = new SparkContext(conf)
 */
     val sc = new SparkContext()
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-    readParameters(args)
+    readParameters(args,sqlContext)
+
+    val ParsedDataModel = Utilities.parseDataModel(sqlContext)
+
+    //Parse Models,Logs and Read Db
+    val ParsedPageWebModel=Utilities.parsePagesWebModel(sqlContext)
+    val ParsedLink=Utilities.parseLink(sqlContext)
+    val Dataframes=DbNames.map(line=>Utilities.parseDbIstance(line,sqlContext)).filter(l=>l!=null)
+    val ParsedWebModel=Utilities.parseViewComponent(sqlContext)
+    val PartialApacheLog=Utilities.parseApacheLogs(sc,sqlContext,ParsedDataModel,ParsedPageWebModel)
+    val EnrichedLog=Utilities.parseRtxLogs(sc,sqlContext,ParsedPageWebModel)
 
 
+    //combine Web model and Data Model
+    val CombinedModels=ParsedWebModel.join(ParsedDataModel, (ParsedWebModel("Entity") === ParsedDataModel("EntityId")))
+    val AddDisplayableAtt_to_CombinedModels=CombinedModels.withColumn("AttributeDisplayable",when(CombinedModels("DisplayedAttribute") ===CombinedModels("AttributeId"), true).otherwise(false))
+    val parsedCombinedModels=AddDisplayableAtt_to_CombinedModels.where("AttributeDisplayable =true").drop("AttributeDisplayable")
+
+    //Enrich Rtx Logs
+    val EnrichedRtx=EnrichedLog.join(parsedCombinedModels,EnrichedLog("unit")===parsedCombinedModels("UnitId"))
+    EnrichedRtx.registerTempTable("EnrichedRtx")
+
+    //Enrich Apache Logs
+    val ApacheLogandLinks=Utilities.enrichApacheLogs(sqlContext,PartialApacheLog,parsedCombinedModels,ParsedLink)
+
+    //get Oid Value from DB
+    val OidDB=Utilities.addOidValueDb(sc,sqlContext)
+
+    //add oid value to Rtx Logs
+    val RtxWithOidDB= EnrichedRtx.join(OidDB, (EnrichedRtx("oid")===OidDB("oidwithDBvalue")) && (EnrichedRtx("TableDB")===OidDB("tableNamewithDBvalue")) &&   EnrichedRtx("AttributeDatabaseColumn")===OidDB("tableColumnwithDBvalue"),"left_outer").withColumnRenamed("Time","TimeRtxLog").withColumnRenamed("sessionId","sessionIdRtx").cache()
+    RtxWithOidDB.registerTempTable("RtxWithOidDB")
+
+    //add oid value to Apache Logs
+    val ApacheWithOidDB= ApacheLogandLinks.join(OidDB, (ApacheLogandLinks("OidValue")===OidDB("oidwithDBvalue")) && (ApacheLogandLinks("TableDB")===OidDB("tableNamewithDBvalue")) &&   ApacheLogandLinks("AttributeDatabaseColumn")===OidDB("tableColumnwithDBvalue"),"left_outer").drop("TableDB").drop("oidwithDBvalue").drop("tableNamewithDBvalue").drop("AttributeDatabaseColumn").withColumnRenamed("tableColumnwithDBvalue","TypeOidClicked").withColumnRenamed("oidDBvalue","ClickedOidDBvalue").dropDuplicates().cache()//.orderBy(asc("Time")).cache()
+    ApacheWithOidDB.registerTempTable("ApacheWithOidDB")
+
+    //Combine Enriched Rtx Logs with Enriched Apache Logs
+    val EnrichedLogsWithDuplicateColumn = ApacheWithOidDB.join(RtxWithOidDB, ApacheWithOidDB("Time")===RtxWithOidDB("TimeRtxLog")
+      && ApacheWithOidDB("SessionId")===RtxWithOidDB("sessionIdRtx")
+      && ApacheWithOidDB("RequestedPage")===RtxWithOidDB("PageId")
+      /*&& ApacheWithOidDB("targetUnit")===RtxWithOidDB("UnitId")*/ ,"left_outer").orderBy(asc("Time"))
+
+
+    //Rinominate some Column
+    val FinalEnrichedLogs=EnrichedLogsWithDuplicateColumn.drop("PageId").drop("unit").drop("sessionIdRtx").drop("TimeRtxLog").drop("EntityId").drop("AttributeId").dropDuplicates().
+      withColumnRenamed("SourcePage","SourcePageId").
+      withColumnRenamed("LinkClicked","ClickedLinkId").
+      withColumnRenamed("RequestedPage","RequestedPageId").
+      withColumnRenamed("OidAttribute","ClickedAttributeId").
+      withColumnRenamed("OidValue","ClickedOid").
+      withColumnRenamed("LinkName","ClickedLinkName").
+      withColumnRenamed("EntityClicked","ClickedEntityName").
+      withColumnRenamed("ClickedOidDBvalue","ClickedOidValue").
+      withColumnRenamed("page","DisplayedPageId").
+      withColumnRenamed("Entity","DisplayedEntityId").
+      withColumnRenamed("UnitName","DisplayedUnitName").
+      withColumnRenamed("EntityName","DisplayedEntityName").
+      withColumnRenamed("DisplayedAttribute","DisplayedAttributeId").
+      withColumnRenamed("AttributeName","DisplayedAttributeName").
+      withColumnRenamed("oidwithDBvalue","DisplayedOid").
+      withColumnRenamed("tableColumnwithDBvalue","DisplayedTypeOid").
+      withColumnRenamed("oidDBvalue","DisplayedOidValue").cache()
+
+
+    //FinalEnrichedLogs.registerTempTable("EnrichedLogs")
+
+    FinalEnrichedLogs.write.mode("append").parquet(basePath+"/FinalEnrichedLogs.parquet")
+
+    //Save EnrichedLog as Hive Table
+    FinalEnrichedLogs.write.mode("append").saveAsTable("EnrichedLogs")
+
+    //perform Analysis On Enriched Logs
+    LogAnalysis.performAnalysis(sc,sqlContext)
   }
-
-  def readParameters(args: Array[String]): Unit ={
-
-    if(args.length>2) {
-      val containerName = args(0)
-      val blobStorageName = args(1)
-      val inputDataPath = args(2)
-      val outputDataPath = args(3)
-
-      //define input and output Cluster blob storage path
-      WebModelpath = "wasb://" + containerName + "@" + blobStorageName + ".blob.core.windows.net/" + inputDataPath + "/doc*.txt"
-      DataModelpath = "wasb://" + containerName + "@" + blobStorageName + ".blob.core.windows.net/" + outputDataPath + "/"
-    }
-    else{
-      val containerName = args(0)
-      val blobStorageName = args(1)
-      val inputDataPath = args(2)
-      val outputDataPath = args(3)
-      WebModelpath = "wasb://" + containerName + "@" + blobStorageName + ".blob.core.windows.net/" + inputDataPath + "/doc*.txt"
-      DataModelpath = "wasb://" + containerName + "@" + blobStorageName + ".blob.core.windows.net/" + outputDataPath + "/"
-
-    }
-  }
-
 
 }
--- src/main/scala/com/webanalytics/config/DataPreparation.scala
+++ src/main/scala/com/webanalytics/config/DataPreparation.scala
@@ -1,22 +1,65 @@
 package com.webanalytics.config
-
+import com.webanalytics.helper._
 import java.text._
 import java.util.Locale
+
+import org.apache.spark.sql.SQLContext
 /**
   * Created by Thanas Koka on 21/02/2017.
   */
 trait DataPreparation {
-  var WebModelpath: String="data/WebModel/**/*/*/page*.wr,data/WebModel/**/*/page*.wr,data/WebModel/**/mpage*.wr,data/WebModel/**/*/*/Properties.wr,data/WebModel/**/*/Properties.wr"
-
+  var basePath= "wasb://tesi@datasettesi.blob.core.windows.net/"
+  var WebModelpath="data/WebModel/**/*/*/page*.wr,data/WebModel/**/*/page*.wr,data/WebModel/**/mpage*.wr,data/WebModel/**/*/*/Properties.wr,data/WebModel/**/*/Properties.wr"
   var DataModelpath="data/DataModel/Properties.wr"
-
   var PersistentDatapath="data/DbIstance/"
-
   var ApacheLogPath="data/dataset-20161216/localhost*.txt"
-
   var RtxLogPath="data//dataset-20161216/RTX.*"
+  var OutputPath="Outputh/"
 
   var  ApacheDateFormat: SimpleDateFormat  = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.ENGLISH)
   var  DateFormat: SimpleDateFormat  = new SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.ENGLISH)
+
+
+  var Units4Att = Array("PowerIndexUnit","IndexUnit","HierarchicalIndexUnit", "DataUnit","MultiDataUnit","EventCalendarUnit","MultiChoiceIndexUnit")
+
+  var Units2Att=Array("ModuleInstanceUnit","MultiMessageUnit","NoOpContentUnit","TimeUnit","GetUnit","NoOpContentUnit")
+  var Units3Att=Array("SelectorUnit","EntryUnit","MultiEntryUnit")
+  var DbNames=Array("AUTHOR","BOOK","BOOK_AUTHOR","BOOK_RELATED_BOOK","CATEGORY","CATEGORY_RELATED_CATEGORY","COMMENT","EDITORIAL_REVIEW","GROUP","GROUP_MODULE","MODULE","ORDER","ORDER_ITEM","USER","USER_GROUP")
+  var Pages = Array("Page","MasterPage")
+  var oldTimestamp: Long=0
+
+
+  def readParameters(args: Array[String],sqlContext: SQLContext): Unit ={
+
+      val containerName = args(0)
+      val blobStorageName = args(1)
+      val WebModelInputPath = args(2)
+      val DataModelInputPath = args(3)
+      val PersistentDataInputPath = args(4)
+      val ApacheLogInputPath=args(5)
+      val RtxLogInputPath=args(6)
+      val OutputParamPath=args(8)
+
+    //define Primary Storage Cluster base path
+      var basePath= "wasb://"+containerName+"@"+blobStorageName+".blob.core.windows.net/"
+
+      //define Sources Cluster blob storage path
+      WebModelpath=basePath+WebModelInputPath+"/**/*/*/page*.wr,"+basePath+"/**/*/page*.wr,"+basePath+"/**/mpage*.wr,"+basePath+"/**/*/*/Properties.wr,"+basePath+"/**/*/Properties.wr"
+      DataModelpath=basePath+DataModelInputPath+"/Properties.wr"
+      PersistentDatapath=basePath+PersistentDataInputPath+"/"
+      ApacheLogPath=basePath+ApacheLogInputPath+"/localhost*.txt"
+      RtxLogPath=basePath+RtxLogInputPath+"/RTX.*"
+      OutputPath=basePath+OutputParamPath+"/"
+
+    try {
+      oldTimestamp = Utilities.readLastTimestampIngestion(sqlContext)
+    }catch{
+      case e => {
+        oldTimestamp= 0
+      }
+    }
+
+  }
+
 
 }
