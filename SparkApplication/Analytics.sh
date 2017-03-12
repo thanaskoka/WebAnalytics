979b4600876ffa9dadd18719a80b8630e4b94888
koka <Cefriel-20!7>
2017-03-12 22:10:30 +0100

    small fixes

--- SparkApplication/Analytics.sh
+++ SparkApplication/Analytics.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 /usr/hdp/current/spark-client/bin/spark-submit \
---class com.webanalytics.analysis.LogEnrichment \
+--class com.webanalytics.analysis.LogAnalysis \
 --name WebAnalytics \
 --master yarn-cluster \
 --driver-memory 10g \
---executor-memory 5G \
+--executor-memory 10G \
 --executor-cores 5 \
 --num-executors 3 \
 WebAnalytics.jar \
-"ContainerName" "BlobStorageName" "WebModelPath" "DataModelPath" "DbPath" "ApacheLogPath" "RtxLogPath" "outputAnalysisPath"
+"wasb://tesi@datasettesi.blob.core.windows.net/data/AnalysisConfiguration.conf"
